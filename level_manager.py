1b20731c2035c9554df7c9ccc6d27568143bbee8
Seongkwan Lee <aksk1119@gmail.com>
2020-12-03 23:18:33 -0700

    Changed how the level change affects the game speed. Might need better way to do this.

--- level_manager.py
+++ level_manager.py
@@ -5,23 +5,17 @@ class LevelManager:
     self.score = 0
     self.level = 0
     self.font = pygame.font.Font(None, 30)
-    self.game_speed = 2
-    self.spawn_interval = 2000
+    self.game_speed = 0.5
+    self.spawn_interval = 2100
 
   def setScore(self):
     self.score += 1
-    if self.score == 5:
-      self.game_speed += 4
-      self.spawn_interval -= 400
-    elif self.score == 10:
-      self.game_speed += 4
-      self.spawn_interval -= 400
-    elif self.score == 20:
-      self.game_speed += 4
-      self.spawn_interval -= 500
-    elif self.score == 40:
-      self.game_speed += 4
-      self.spawn_interval -= 500
+    if self.score % 10 == 0:
+      self.level += 1
+      self.game_speed += 0.3
+      if self.spawn_interval > 100:
+        self.spawn_interval -= 400
+        
 
   def getLevel(self)-> int:
     return self.level
@@ -32,3 +26,9 @@ class LevelManager:
 
     score_obj = self.font.render("Level: " + str(self.level), True, (255,255,255))
     screen.blit(score_obj,(10, 50))
+  
+  def gameover(self, screen):
+    screen.fill((0,0,0))
+    message = self.font.render("Game Over", True, (255,255,255))
+    screen.blit(message,(10, 320))
+    pygame.display.update()
